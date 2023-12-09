49a39e71928d2be6a0cb3f5baaa0bdaa9117ce70
Sourav Halder <halder.sourav1996@gmail.com>
2023-12-09 23:04:01 +0530

    Data Ingestion added

--- README.md
+++ README.md
@@ -8,7 +8,8 @@
 3. Update params.yaml
 4. Update the entity
 5. Update the configuration manager in src config
-6. Update the pipeline
+6. Update the components
+7. Update the pipeline
 8. Update the main.py
 9. Update the dvc.yaml
 10. app.py
--- main.py
+++ main.py
@@ -1,4 +1,17 @@
 from src.Kidney_Disease_Classifier import logger
+from src.Kidney_Disease_Classifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
-logger.info("This is my first custom log test.")
+
+STAGE_NAME = "Data Ingestion"
+
+try:
+    logger.info(f">>>>> Stage {STAGE_NAME} Started <<<<<")
+    obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>> Stage {STAGE_NAME} Completed <<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
 
