0e8a6604eb133b6c3b25e8aed031e7ca8799b97a
Sourav Halder <halder.sourav1996@gmail.com>
2023-12-11 16:34:47 +0530

    Model Training Added

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from src.Kidney_Disease_Classifier import logger
 from src.Kidney_Disease_Classifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from src.Kidney_Disease_Classifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelPipeline
+from src.Kidney_Disease_Classifier.pipeline.stage_03_model_training import ModelTrainingPipeline
+
 
 STAGE_NAME = "Data Ingestion"
 
@@ -27,5 +29,18 @@ except Exception as e:
     logger.exception(e)
     raise e
 
+
+
+STAGE_NAME = "Training"
+
+try:
+    logger.info(f"******************************")
+    logger.info(f">>>>> Stage {STAGE_NAME} Started <<<<<")
+    model_trainer = ModelTrainingPipeline()
+    model_trainer.main()
+    logger.info(f">>>>> Stage {STAGE_NAME} Completed <<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
 
 
